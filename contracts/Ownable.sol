22acf8a7d01e0bc9b7a346e7ef9e66f62764db83
sahitya kumar suman <sahityakumarsuman@gmail.com>
2018-11-14 14:19:48 +0530

    Initial Commit

---
+++ contracts/Ownable.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.18;
+
+contract Ownable {
+
+    // state variable
+    address owner;
+
+    // modifier
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    // constructor
+    function Ownable() public {
+        owner = msg.sender;
+    }
+}
---
+++ private/startnode.sh
@@ -0,0 +1,1 @@
+geth --networkid 4224 --mine --minerthreads 1 --datadir "/Users/admin/Desktop/DappsWork/chainlist/private" --nodiscover --rpc --rpcport "8545" --port "30303" --rpccorsdomain "*" --nat "any" --rpcapi eth,web3,personal,net --unlock 0 --password /Users/admin/Desktop/DappsWork/chainlist/private/password.sec --ipcpath "~/Library/Ethereum/geth.ipc"
